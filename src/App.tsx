1d88fd1d5936481853bc9f4f33451d6e94bc391d
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-10-29 03:35:41 +0300

    configured the navgation panel

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,29 @@ import {
 
 /**
  * Render the main application component.
- * 
+ *
  * @returns {JSX.Element} The rendered application component.
  */
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       {/* Navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       {/* Hero */}
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       {/* Main content */}
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar />
-          <Hero />
           <Billing />
           <Business />
           <CardDeal />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constant";
+
+/**
+ * Navbar component
+ *
+ * This component renders a navigation bar with a logo, a list of links, and a toggle button.
+ * The toggle button changes the visibility of the links on small screens.
+ */
 export const Navbar = () => {
-    return (
-      <div>Navbar</div>
-    )
-  }
+  // State to track the toggle button
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      {/* Logo */}
+      <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+      {/* Navigation links */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      {/* Toggle button */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-p-posts font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+              </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
